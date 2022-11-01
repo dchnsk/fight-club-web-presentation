0861046c42d444af5826b13f50293d9b2d879ea3
dchnsk <leriidchnsk@gmail.com>
2022-11-01 22:30:28 +0300

    Modify team members cards

--- src/components/about/TeamSection.jsx
+++ src/components/about/TeamSection.jsx
@@ -24,16 +24,22 @@ export const TeamSection = () => (
                 "
             >
                 {Contents.teamMembers.map((person) => (
-                    <div key={person.id} className="text-primary flex flex-col items-center m-4">
+                    <div key={person.id} className="text-primary flex flex-col w-[100px] items-center m-5">
                         <div
                             className="
-                                bg-unknown-person bg-contain bg-[right_bottom_-8px] bg-hightlight
+                                bg-unknown-person bg-contain bg-[right_bottom_-7px] bg-hightlight
                                 w-[100px] h-[100px]  
-                                rounded-full
+                                border-hightlight border-[0.25rem]
+                                overflow-hidden
+                                rounded-full 
                             "
-                        ></div>
-                        <span className="text-xl">{person.name}</span>
-                        <span>{person.position}</span>
+                        >
+                            {person.image && (
+                                <img src={person.image} alt={person.name} className="w-full h-full object-cover" />
+                            )}
+                        </div>
+                        <span className="text-xl overflow-hidden text-center">{person.name}</span>
+                        <span className="opacity-60 text-center">{person.position}</span>
                     </div>
                 ))}
             </div>
